23b0df95a9242116149093fa471984dda8f1bd63
itggot-Ellinor-Ornvall <ellinor.ornvall@itggot.se>
2018-04-25 13:58:18 +0200

    Added documentation

--- lib/between_strict.rb
+++ lib/between_strict.rb
@@ -1,3 +1,7 @@
+# number x- första numret av 3 som ska jämföras för att ta reda på om det första ligger strikt före andra och tredje 
+# number y- andra numret av 3 som ska jämföras för att ta reda på om det första ligger strikt före andra och tredje
+# number z- tredje numret av 3 som ska jämföras för att ta reda på om det första ligger strikt före andra och tredje
+
 def between_strict(x, y, z)
     if y < z
             min = y
