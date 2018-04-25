b65731ffa722283e6f108d15e48313825970fa39
itggot-Ellinor-Ornvall <ellinor.ornvall@itggot.se>
2018-04-25 14:09:47 +0200

    Added documentation

--- lib/between.rb
+++ lib/between.rb
@@ -1,3 +1,7 @@
+# number x- första numret av 3 som ska jämföras för att ta reda på om den första ligger mellan den andra och tredje
+# number y- andra numret av 3 som ska jämföras för att ta reda på om den första ligger mellan den andra och tredje
+# number z- tredje numret av 3 som ska jämföras för att ta reda på om den första ligger mellan den andra och tredje
+
 def between(x, y, z)
     if y < z
         min = y
