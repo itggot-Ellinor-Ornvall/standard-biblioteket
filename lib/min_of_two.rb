661c9b33db1c5776a78a461ca490aa2f0e61ebe7
itggot-Ellinor-Ornvall <ellinor.ornvall@itggot.se>
2018-04-25 14:18:11 +0200

    Added documentation

--- lib/min_of_two.rb
+++ lib/min_of_two.rb
@@ -1,3 +1,6 @@
+# number x- det tal som ska jämföras med ett annat tal för att avgöra vilket av de två talen som är minst
+# number y- det tal som ska jämföras med ett annat tal för att avgöra vilket av de två talen som är minst
+
 def min_of_two(x, y)
     if x<y
         return x
